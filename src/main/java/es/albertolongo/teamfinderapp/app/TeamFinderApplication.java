97d3d53dff0645018c4126d0ebc46590432f267e
Alberto Longo Gómez <alg00073@red.ujaen.es>
2022-03-15 13:44:47 +0100

    Initial Commit

---
+++ src/main/java/es/albertolongo/teamfinderapp/app/TeamFinderApplication.java
@@ -0,0 +1,12 @@
+package es.albertolongo.teamfinderapp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamFinderApplication.class, args);
+    }
+}
---
+++ src/test/java/es/albertolongo/teamfinderapp/app/TeamFinderAppTests.java
@@ -0,0 +1,13 @@
+package es.albertolongo.teamfinderapp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamFinderAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
