5256f55cb24724df19b5d358e3f049f60cf8ba89
Alberto Longo Gómez <alg00073@red.ujaen.es>
2022-03-21 18:13:21 +0100

    Meter api-v6, crear entidad Game, Role, Rank, cambiar preferencias

--- src/main/java/es/albertolongo/teamfinderapp/TeamFinderApplication.java
+++ src/main/java/es/albertolongo/teamfinderapp/TeamFinderApplication.java
@@ -1,12 +1,28 @@
 package es.albertolongo.teamfinderapp;
 
+import es.albertolongo.teamfinderapp.service.PlayerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TeamFinderApplication {
+public class TeamFinderApplication implements CommandLineRunner {
+
+    @Autowired
+    PlayerService playerService;
+
+    private static Logger LOG = LoggerFactory
+            .getLogger(TeamFinderApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(TeamFinderApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("Mensaje de prueba");
     }
 }
